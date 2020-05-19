02099a361e6633aae95e6fabb83475670e68fec5
Jaxel Rojas <jaxelr@gmail.com>
2020-05-19 00:08:43 -0400

    refactor to theory and member data

--- MainTest.cs
+++ MainTest.cs
@@ -2,21 +2,32 @@ using Npgsql;
 using System;
 using Xunit;
 using Dapper;
+using System.Collections.Generic;
 
 namespace NpgSampleAppVeyorTests
 {
     public class MainTest
     {
-        [Fact]
-        public void Test1()
+        public static IEnumerable<object[]> GetData()
         {
+            //We do this to pass the connection from Appveyor or locally
             string postgresConnEnv = Environment.GetEnvironmentVariable("Postgres_Connection");
-            if (string.IsNullOrEmpty(postgresConnEnv))
+            if(string.IsNullOrEmpty(postgresConnEnv))
             {
-                postgresConnEnv = $"Server=localhost;Port=5432;Database=postgres;Integrated Security=true;Username=postgres";
+                postgresConnEnv = $"Server=localhost;Port=5432;Database=postgres;Integrated Security=true;Username=postgres;Password=postgres;";
             }
 
-            var conn = new NpgsqlConnection(postgresConnEnv);
+            return new List<object[]>
+            {
+                new object[] { postgresConnEnv }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test1(string connectionString)
+        {
+            var conn = new NpgsqlConnection(connectionString);
 
             conn.Open();
 
